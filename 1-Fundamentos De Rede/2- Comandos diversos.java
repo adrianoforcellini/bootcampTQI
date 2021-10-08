f1bd9d6066c0411ccd4cfafdfb005995b3739c0d
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-08 12:01:32 -0300

    Comandos Linux

---
+++ 1-Fundamentos De Rede/2- Comandos diversos.java
@@ -0,0 +1,27 @@
+// Exibir informações sobre reboot do sistema
+// last reboot
+
+// Tabelas de Roteamento IP do Kernel
+// route -n
+
+// Exibir o tempo que um comando demora para ser executado
+// time ${comando}
+
+// Informações sobre horario e carregamento do sistema
+// uptime
+
+
+//cowsay "To mentindo? Falo Mermo."
+//  __________________________
+// < To mentindo? Falo Mermo. >
+// --------------------------
+//        \   ^__^
+//         \  (oo)\_______
+//            (__)\       )\/\
+//                ||----w |
+//                ||     ||
+
+// cowsay -f tux "mensagem"
+// cowsay -f vader..
+// cowsay -g ...
+// cowsay -f dragon .... ( esse é foda )
